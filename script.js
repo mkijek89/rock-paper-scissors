2ba7a6c04cebe373a3b6a80a812257fc82ccc3c3
mkijek89 <misiex131@gmail.com>
2022-09-01 23:11:04 +0200

    Add userChoice and compareChoices

--- script.js
+++ script.js
@@ -4,4 +4,82 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random()* choices.length)];
 }
 
-console.log(getComputerChoice());
+function compareChoices(a,b) {
+    switch (a) {
+        case 'rock':
+            if (b == "rock"){
+                return "It's a draw";
+            }
+            else if (b == 'scissors'){
+                return "Rock beats scissors, computer wins!";
+            }
+            else {
+                return "Paper beats rock, you win!";
+            }
+            break;
+
+        case 'scissors':
+            if (b == "rock"){
+                return "Rock beats scissors, you win!";
+            }
+            else if (b == 'scissors'){
+                return "It's a draw";
+            }
+            else {
+                return "Scissors beat paper, computer wins!";
+            }
+            break;
+
+        case 'paper':
+            if (b == "rock"){
+                return "Paper beats rock, computer wins!";
+            }
+            else if (b == 'scissors'){
+                return "Scissors beat paper, you win!";
+            }
+            else {
+                return "It's a draw";
+            }
+            break;
+    }
+
+}
+
+const choices = ['rock', 'paper', 'scissors'];
+
+
+
+
+
+for (let i = 0; i < 5; i++) {
+    
+    let userChoice = prompt("Please enter your choice")
+
+    if (choices.includes(userChoice)) {
+        console.log('OK');
+    }
+    
+    else {
+        while (!(choices.includes(userChoice)))
+    userChoice = prompt("You have to enter \"rock\", \"paper\" or \"scissors\"");
+    }
+
+    compareChoices(getComputerChoice(), userChoice)
+
+    if (compareChoices(getComputerChoice(), userChoice).includes('computer')){
+        i++;
+        console.log(compareChoices(getComputerChoice(), userChoice));
+    }
+    
+   
+    console.log(i);
+    
+}
+
+
+
+
+
+
+
+
