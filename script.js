4c987d16aa54644d05a5baaa809d423616f26cea
mkijek89 <misiex131@gmail.com>
2022-09-01 22:25:37 +0200

    Add boilerplate and computer choice

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+
+    return choices[Math.floor(Math.random()* choices.length)];
+}
+
+console.log(getComputerChoice());
